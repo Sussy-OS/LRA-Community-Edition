c1bdf6c0b24d4b3b757761e6c31287002503fe6d
Sussy Inc <SussyS0S-Operating-Systems@protonmail.com>
2022-03-08 16:12:48 -0500

    Update EditVersion.py

--- EditVersion.py
+++ EditVersion.py
@@ -4,11 +4,3 @@ import sys
 
 with open('version', "w") as version:
     version.write(sys.argv[1])
-
-with open('src/Program.cs','r',encoding='utf-8') as file:
-    data = file.readlines()
-    
-data[38] = "        public static string Version = "sys.argv[1]";\n"
-
-with open('src/Program.cs', 'w', encoding='utf-8') as file:
-    file.writelines(data)
