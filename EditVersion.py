f570e7a323c0f1385115c869ae1e41ede48dde46
Sussy Inc <SussyS0S-Operating-Systems@protonmail.com>
2022-03-08 15:28:50 -0500

    Update EditVersion.py

--- EditVersion.py
+++ EditVersion.py
@@ -8,8 +8,10 @@ with open('version', "w") as version:
     
     with open("src/UI/Dialogs/ChangelogWindow.cs", "r") as f:
         contents = f.readlines()
+        mySeparator = "TEST"
+        x = mySeparator.join(sys.argv[2])
 
-    contents.insert(24, sys.argv[1])
+    contents.insert(24, x)
 
     with open("src/UI/Dialogs/ChangelogWindow.cs", "w") as f:
         contents = "".join(contents)
